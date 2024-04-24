4fe75c9a7204d462b620069fca225c8b548ac2ea
Sprang, Richard <Richard .sprang.rs@gmail.com>
2024-04-24 17:18:01 +0200

    changed password textfield to actual password field

--- src/main/java/com/iu/JaWa/ui/MainView.java
+++ src/main/java/com/iu/JaWa/ui/MainView.java
@@ -3,6 +3,7 @@ package com.iu.JaWa.ui;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -12,9 +13,8 @@ public class MainView extends VerticalLayout {
 	private static final long serialVersionUID = 1L;
 
 	public MainView() {
-		
-		TextField userName = new TextField(), password = new TextField();
-		
+		TextField userName = new TextField();
+		PasswordField password = new PasswordField();
 		add(userName);
 		add(password);
 		add(new Button("Login", e -> Notification.show(userName.getValue()+" "+password.getValue())));
