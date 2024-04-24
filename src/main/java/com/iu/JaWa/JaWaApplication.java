a2bdf5510ec2646f00de27c9ed73173c620ed3d9
Sprang, Richard <Richard .sprang.rs@gmail.com>
2024-04-24 16:01:58 +0200

    Initial Commit

---
+++ src/main/java/com/iu/JaWa/JaWaApplication.java
@@ -0,0 +1,13 @@
+package com.iu.JaWa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaWaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaWaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/JaWa/JaWaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.JaWa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaWaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
